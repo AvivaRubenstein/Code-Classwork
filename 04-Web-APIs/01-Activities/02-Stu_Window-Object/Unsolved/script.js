0bdcd36511d1bbd9b170449a86ffa413c7ebc986
AvivaRubenstein <avivadrubenstein@gmail.com>
2022-11-21 22:04:51 -0500

    classwork

---
+++ 04-Web-APIs/01-Activities/02-Stu_Window-Object/Unsolved/script.js
@@ -0,0 +1,18 @@
+// TODO: What will the following line of code log?
+//access to everything in the window
+console.log(window);
+
+// TODO: What will the following line of code log?
+//gives us access to the entire html document
+console.log(window.document);
+
+// TODO: What will the following line of code log?
+//brings us to the root element (in this case -HTML)
+//really just takes out the DocType HTML at the top
+console.log(document.documentElement);
+
+// TODO: What will the following line of code log?
+//this will log everything in the head element
+//document.body will log everything in the body instead
+console.log(document.head);
+
